6bdcdb8c59821dd0dda745d5a6634162a527fa39
jirving1 <jrving1@gmail.com>
2025-02-14 10:58:20 -0500

    solves maze succesfully

--- maze.py
+++ maze.py
@@ -70,7 +70,7 @@ class Maze:
         while True:
             next_index_list = []
 
-            # determine which cell(s) to visit next
+            
             # left
             if i > 0 and not self._cells[i - 1][j].visited:
                 next_index_list.append((i - 1, j))
@@ -84,17 +84,16 @@ class Maze:
             if j < self._num_rows - 1 and not self._cells[i][j + 1].visited:
                 next_index_list.append((i, j + 1))
 
-            # if there is nowhere to go from here
-            # just break out
+           
             if len(next_index_list) == 0:
                 self._draw_cell(i, j)
                 return
 
-            # randomly choose the next direction to go
+            
             direction_index = random.randrange(len(next_index_list))
             next_index = next_index_list[direction_index]
 
-            # knock out walls between this cell and the next cell(s)
+            
             # right
             if next_index[0] == i + 1:
                 self._cells[i][j].has_right_wall = False
@@ -112,7 +111,7 @@ class Maze:
                 self._cells[i][j].has_top_wall = False
                 self._cells[i][j - 1].has_bottom_wall = False
 
-            # recursively visit the next cell
+           
             self._break_walls_r(next_index[0], next_index[1])
         
     def _reset_cells_visited(self):
@@ -123,14 +122,14 @@ class Maze:
     def _solve_r(self, i, j):
         self._animate()
 
-        # vist the current cell
+       
         self._cells[i][j].visited = True
 
-        # if we are at the end cell, we are done!
+        
         if i == self._num_cols - 1 and j == self._num_rows - 1:
             return True
 
-        # move left if there is no wall and it hasn't been visited
+        
         if (
             i > 0
             and not self._cells[i][j].has_left_wall
@@ -142,7 +141,7 @@ class Maze:
             else:
                 self._cells[i][j].draw_move(self._cells[i - 1][j], True)
 
-        # move right if there is no wall and it hasn't been visited
+
         if (
             i < self._num_cols - 1
             and not self._cells[i][j].has_right_wall
@@ -154,7 +153,7 @@ class Maze:
             else:
                 self._cells[i][j].draw_move(self._cells[i + 1][j], True)
 
-        # move up if there is no wall and it hasn't been visited
+        
         if (
             j > 0
             and not self._cells[i][j].has_top_wall
@@ -166,7 +165,7 @@ class Maze:
             else:
                 self._cells[i][j].draw_move(self._cells[i][j - 1], True)
 
-        # move down if there is no wall and it hasn't been visited
+        
         if (
             j < self._num_rows - 1
             and not self._cells[i][j].has_bottom_wall
@@ -178,7 +177,7 @@ class Maze:
             else:
                 self._cells[i][j].draw_move(self._cells[i][j + 1], True)
 
-        # we went the wrong way let the previous cell know by returning False
+        
         return False
 
     
